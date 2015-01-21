86586e407b4a6c66096476b73d70101cd966a888
Alex Orlenko <eaglexrlnk@gmail.com>
2015-01-21 19:16:11 +0200

    Add expression interpreter

---
+++ Interpreter.hs
@@ -0,0 +1,21 @@
+module Interpreter (evalExpr) where
+
+import Control.Applicative
+import Control.Monad.Error
+
+import Parser
+
+evalExpr :: AST -> Either String Double
+evalExpr (ASTExpression e) = evalExpr e
+evalExpr (ASTNumber n) = return n
+evalExpr (ASTPlus l r) = (+) <$> evalExpr l <*> evalExpr r
+evalExpr (ASTMinus l r) = (-) <$> evalExpr l <*> evalExpr r
+evalExpr (ASTMultiply l r) = (*) <$> evalExpr l <*> evalExpr r
+evalExpr (ASTPower l r) = (**) <$> evalExpr l <*> evalExpr r
+evalExpr (ASTDivide l r) = do left <- evalExpr l
+                              right <- evalExpr r
+                              if right == 0
+                                 then Left "division by zero"
+                                 else return $ left / right
+
+evalExpr _ = Left "incorrect expression"
